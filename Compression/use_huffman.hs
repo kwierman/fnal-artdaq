d906f76dd311510f7938e1ce4f372da2ce4547d8
Marc Paterno <paterno@fnal.gov>
2011-12-13 14:26:49 -0600

    Adding Haskell implementation

---
+++ Compression/use_huffman.hs
@@ -0,0 +1,13 @@
+import Huffman
+
+main = do
+     putStrLn . show $ encodePhrase "0"
+     putStrLn . show $ encodePhrase "1"
+     putStrLn . show $ encodePhrase "2"
+     putStrLn . show $ encodePhrase "3"
+     putStrLn . show $ encodePhrase "4"
+     putStrLn . show $ encodePhrase "5"
+     putStrLn . show $ encodePhrase "6"
+     putStrLn . show $ encodePhrase "7"
+     putStrLn . show $ encodePhrase "55274630607402075163066705471072677564736"
+     putStrLn . show $ encodePhrase "01234567"
