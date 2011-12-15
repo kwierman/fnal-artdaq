1cd6a4df668dab4ce90f7009af5cbd4141b2e04a
Ron Rechenmacher <ron@fnal.gov>
2011-12-15 13:39:13 -0600

    reading from file

--- DAQrate/EventPool.cc
+++ DAQrate/EventPool.cc
@@ -16,6 +16,8 @@ FragmentPool::FragmentPool(Config const& conf):
   std::ostringstream name;
   name << "board" << conf.offset_ << ".out";
   ifs_.open( name.str().c_str(), std::ifstream::in );
+  std::cout << "name is " << name.str() << '\n';
+  std::cout << "ifs_.is_open() is " << ifs_.is_open() << '\n';
   generate(d_.begin(),d_.end(),LongMaker());
 }
 
@@ -27,11 +29,30 @@ void FragmentPool::operator()(Data& output)
 {
   if(output.size()<word_count_) output.resize(word_count_);
 
-  //int start = (LongMaker::make()) % range_;
-  int start = 0;
+  int start = (LongMaker::make()) % range_;
   std::copy(d_.begin()+start, d_.begin()+start+word_count_, output.begin());
-
   FragHeader* h = (FragHeader*)&output[0];
+#if 1
+  if (ifs_.eof())
+    ifs_.seekg(0, std::ios::beg);
+
+  char *cp=(char*)&output[0];
+  std::cout << "output.size()="<<output.size()<<" bytes="<<output.size()*4<<'\n';
+  ifs_.read( cp, sizeof(FragHeader) );
+
+  int size=(h->frag_words_ & 0xfffffff);
+
+  if (output.size() < size)
+  {output.resize(size);
+    cp=(char*)&output[0];
+    h = (FragHeader*)&output[0];
+  }
+  size *= sizeof(int);
+  printf("frag size=%d fragwords=0x%x\n", size, h->frag_words_ );
+  size -= sizeof(FragHeader);
+  ifs_.read( cp+sizeof(FragHeader), size );
+ 
+#endif
 
   h->id_=seq_++;
   h->from_=rank_;
