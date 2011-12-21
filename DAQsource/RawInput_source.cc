9e10769a640a4965018f7aec6c3ee60d21bdaf39
Jim Kowalkowski <jbk@fnal.gov>
2011-12-21 07:16:46 -0600

    more implementation added

--- DAQsource/RawInput_source.cc
+++ DAQsource/RawInput_source.cc
@@ -18,6 +18,7 @@ namespace artdaq
   {
     art::PrincipalMaker const& pm_;
     RawEventQueue& queue_;
+    const vector<string> inst_names_;
 
     EventStoreReader(fhicl::ParameterSet const& ps,
 		     art::ProductRegistryHelper& help,
@@ -37,16 +38,14 @@ namespace artdaq
 				     art::ProductRegistryHelper& help,
 				     art::PrincipalMaker const& pm):
     pm_(pm),
-    queue_(getQueue())
+    queue_(getQueue()),
+    inst_name_(ps.get<vector<string>>("instances"))
   {
-    const vector<string> inst_names = ps.get<vector<string>>("instances");
-    for_each(inst_names.cbegin(),inst_names.cend(),
+    for_each(inst_names_.cbegin(),inst_names_.cend(),
 	     [&](string const& iname)
 	     {
 	       help.reconstitutes<Fragment, art::InEvent>("RawInput",iname);
 	     });
-
-    
   }
 
   void EventStoreReader::closeCurrentFile()
@@ -59,7 +58,7 @@ namespace artdaq
     fb = new FileBlock(FileFormatVersion(1,"RawEvent2011"),"nothing");
   }
     
-  bool EventStreReader::readNext(art::RunPrincipal* const& inR,
+  bool EventStoreReader::readNext(art::RunPrincipal* const& inR,
 				 art::SubRunPrincipal* const& inSR,
 				 art::RunPrincipal*& outR,
 				 art::SubRunPrincipal*& outSR,
@@ -68,24 +67,56 @@ namespace artdaq
     RawEvent_ptr p;
     queue_.deqWait(p);
 
-    if(!p)
-      {
-	// end of data stream
-	return false;
-      }
+    // check for end of data stream
+    if(!p) { return false; }
 
     Timestamp runstart;
 
-    if(inR==0) outR=pm_.makeRunPrincipal();
-    if(inSR==0) outSR=pm_.makeSubRunPrincipal();
+    // make new runs or subruns if in* are 0 or if the run/subrun
+    // have changed
 
-    outE = pm_.makeEventPrincipal(inR==0?outR->run():inR->run(),
-				  inSR==0?outSR->subRun():inSR->subRun(),
+    if(inR==0 || inR->run()!=p->header_.run_id_)
+      {
+	outR=pm_.makeRunPrincipal(p->header_.run_id_,
+				  runstart);
+      }
+
+    art::SubRunID subrun_check(p->header_.run_id_, p->header_.subrun_id_);
+
+    if(inSR==0 || subrun_check!=inSR.id())
+      {
+	outSR=pm_.makeSubRunPrincipal(p->header_.run_id_,
+				      p->header_.subrun_id_,
+				      runstart);
+      }
+
+    outE = pm_.makeEventPrincipal(p->header_.run_id_,
+				  p->header_.subrun_id_,
 				  p->header_.event_id_,
 				  runstart);
 
     // add all the fragments as products
-    
+    if(inst_name_.size() < p->fragment_list_.size())
+      {
+	throw Exception("RawData")
+	  << "more raw data fragments than expected.\n"
+	  << "expected " << inst_name_.size() << " and got "
+	  << p->fragment_list_.size() << "\n"
+	  << "for event " << p->header_.run_d_;
+      }
+
+    for(auto c=p->fragment_list_.begin(),e=p->fragment_list_.end();
+	c!=e;++c)
+      {
+	auto_ptr<Fragment> frag(c->release());
+	put_product_in_principal(frag,*outE,inst_names_[i]);
+      }
+
+    /* example
+       std::auto_ptr<std::vector<rawdata::FlatDAQData> > flatcol(new std::vector<rawdata::FlatDAQData>);
+       flatcol->push_back(flat_daq_data); 
+       art::put_product_in_principal(flatcol,*outE,"module","inst");
+    */
   }
 
   typedef art::ReaderSource<EventStoreReader> RawInput;
