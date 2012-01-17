8d28eee866176cea7ebb013bb02476ffbeaa749b
Jim Kowalkowski <jbk@fnal.gov>
2012-01-17 11:09:42 -0600

    add pseudo-code for driving the DS reader and Eventstore.

--- DAQsource/driver.cc
+++ DAQsource/driver.cc
@@ -23,13 +23,35 @@ int main(int argc, char* argv[])
   fhicl::make_ParameterSet(argv[1],gunk,pset);
   fhicl::ParameterSet ds_pset = pset.getParameterSet("ds50");
 
-  artdaq::DS50Reader reader(ds_pset);
+  artdaq::DS50EventReader reader(ds_pset);
 
   artdaq::EventStore store(ds_pset.get<int>("run"),
 			   ds_pset.get<int>("source_count"),
 			   argc,argv);
 
-  // reader.run(store);
+  // simple way - speed depends on I/I reading
+  Fragments el;
+  while(reader.getNext(el))
+    {
+      for_each(el.begin(),el.end(),
+	       [&](Fragments::value_type& val) { store.insert(val); }
+	       );
+    }
+
+  // buffering way
+  vector<Fragments> event_buffer;
+  int total_events = ds_pset.get<int>("total_events");
+  Fragments event;
+
+  while(reader.getNext(event) && total_events>0)
+    {
+      event_buffer.push_back(event);
+      --total_events;
+    }
+
+  for_each(event_buffer.begin(),event_buffer.end(),
+	   [&](Fragments& val) { /* go through each fragment and put it onto the store queue */ }
+	   );
 
   return 0;
 }
