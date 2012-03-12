1ca29bd50d4f4a216697fc6cb19501a0e0ecec5d
Chris Green <artists@fnal.gov>
2012-03-12 13:53:27 -0500

    Abstract interface test.

---
+++ test/DAQrate/FragmentGenerator_t.cc
@@ -0,0 +1,42 @@
+#define BOOST_TEST_MODULE ( FargmentGenerator_t )
+#include "boost/test/auto_unit_test.hpp"
+
+#include "artdaq/DAQdata/Fragment.hh"
+#include "artdaq/DAQrate/FragmentGenerator.hh"
+
+namespace artdaqtest {
+  class FragmentGeneratorTest;
+}
+
+class artdaqtest::FragmentGeneratorTest :
+  public artdaq::FragmentGenerator {
+public:
+  FragmentGeneratorTest();
+private:
+  bool getNext_(artdaq::FragmentPtrs &);
+};
+
+artdaqtest::FragmentGeneratorTest::FragmentGeneratorTest()
+  :
+  FragmentGenerator()
+{
+}
+
+bool
+artdaqtest::FragmentGeneratorTest::getNext_(artdaq::FragmentPtrs &frags) {
+  frags.emplace_back(new artdaq::Fragment);
+  return true;
+}
+
+BOOST_AUTO_TEST_SUITE(FragmentGenerator_t)
+
+BOOST_AUTO_TEST_CASE(Simple)
+{
+  artdaqtest::FragmentGeneratorTest testGen;
+  artdaq::FragmentGenerator & baseGen(testGen);
+  artdaq::FragmentPtrs fps;
+  baseGen.getNext(fps);
+  BOOST_REQUIRE_EQUAL(fps.size(), 1u);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
