1c5e819fda41cab40fc976c9ba1295a534a4a215
Marc Paterno <paterno@fnal.gov>
2012-03-12 10:16:40 -0500

    Checkpoint

--- artdaq/DAQsource/DS50RawInput_source.cc
+++ artdaq/DAQsource/DS50RawInput_source.cc
@@ -22,98 +22,118 @@ using std::vector;
 using std::auto_ptr;
 
 namespace artdaq {
-  struct RawEventQueueReader {
-    art::PrincipalMaker const & pm_;
-    RawEventQueue &             queue_;
-    vector<string> const        inst_names_;
-    daqrate::seconds            waiting_time_;
-    bool                        resume_after_timeout_;
+  namespace detail {
+    struct RawEventQueueReader {
+      RawEventQueueReader(RawEventQueueReader const&) = delete;
+      RawEventQueueReader& operator=(RawEventQueueReader const&) = delete;
 
-    RawEventQueueReader(fhicl::ParameterSet const & ps,
-                        art::ProductRegistryHelper & help,
-                        art::PrincipalMaker const & pm);
+      art::PrincipalMaker const & pmaker;
+      RawEventQueue &             incoming_events;
+      vector<string> const        product_instance_names;
+      daqrate::seconds            waiting_time;
+      bool                        resume_after_timeout;
 
-    void closeCurrentFile();
-    void readFile(string const & name, art::FileBlock* & fb);
+      RawEventQueueReader(fhicl::ParameterSet const & ps,
+                          art::ProductRegistryHelper & help,
+                          art::PrincipalMaker const & pm);
 
-    bool readNext(art::RunPrincipal* const & inR,
-                  art::SubRunPrincipal* const & inSR,
-                  art::RunPrincipal* & outR,
-                  art::SubRunPrincipal* & outSR,
-                  art::EventPrincipal* & outE);
-  };
+      void closeCurrentFile();
+      void readFile(string const & name, art::FileBlock* & fb);
 
-  RawEventQueueReader::RawEventQueueReader(fhicl::ParameterSet const & ps,
-                                           art::ProductRegistryHelper & help,
-                                           art::PrincipalMaker const & pm):
-    pm_(pm),
-    queue_(getGlobalQueue()),
-    inst_names_(ps.get<vector<string>>("instances")),
-    waiting_time_(ps.get<double>("waiting_time", std::numeric_limits<double>::infinity())),
-    resume_after_timeout_(ps.get<bool>("resume_after_timeout", true))
-  {
-    for_each(inst_names_.cbegin(), inst_names_.cend(),
-             [&](string const & iname) {
-               help.reconstitutes<Fragment, art::InEvent>("DS50RawInput", iname);
-             });
+      bool readNext(art::RunPrincipal* const & inR,
+                    art::SubRunPrincipal* const & inSR,
+                    art::RunPrincipal* & outR,
+                    art::SubRunPrincipal* & outSR,
+                    art::EventPrincipal* & outE);
+    };
   }
 
-  void RawEventQueueReader::closeCurrentFile()
+  detail::RawEventQueueReader::RawEventQueueReader(fhicl::ParameterSet const & ps,
+                                                   art::ProductRegistryHelper & help,
+                                                   art::PrincipalMaker const & pm):
+    pmaker(pm),
+    incoming_events(getGlobalQueue()),
+    product_instance_names(ps.get<vector<string>>("instances")),
+    waiting_time(ps.get<double>("waiting_time", std::numeric_limits<double>::infinity())),
+    resume_after_timeout(ps.get<bool>("resume_after_timeout", true))
+  {
+    for (auto const& iname : product_instance_names)
+      help.reconstitutes<Fragment, art::InEvent>("DS50RawInput", iname);
+//     for_each(product_instance_names.cbegin(), product_instance_names.cend(),
+//              [&](string const & iname) {
+//                help.reconstitutes<Fragment, art::InEvent>("DS50RawInput", iname);
+//              });
+  }
+
+  inline
+  void detail::RawEventQueueReader::closeCurrentFile()
   {
   }
 
-  void RawEventQueueReader::readFile(string const & /* name */,
+  void detail::RawEventQueueReader::readFile(string const & /* name */,
                                      art::FileBlock* & fb)
   {
     fb = new art::FileBlock(art::FileFormatVersion(1, "RawEvent2011"), "nothing");
   }
 
-  bool RawEventQueueReader::readNext(art::RunPrincipal* const & inR,
-                                     art::SubRunPrincipal* const & inSR,
-                                     art::RunPrincipal* & outR,
-                                     art::SubRunPrincipal* & outSR,
-                                     art::EventPrincipal* & outE)
+  bool detailRawEventQueueReader::readNext(art::RunPrincipal* const & inR,
+                                           art::SubRunPrincipal* const & inSR,
+                                           art::RunPrincipal* & outR,
+                                           art::SubRunPrincipal* & outSR,
+                                           art::EventPrincipal* & outE)
   {
     RawEvent_ptr p;
 
-    // Try to get an event from the queue ...
+    // Try to get an event from the queue. We'll continuously loop, either until:
+    //   1) we have read a RawEvent off the queue, or
+    //   2) we have timed out, AND we are told the when we timeout we
+    //      should stop.
+    // In any case, if we time out, we emit an informational message.
     bool keep_looping = true;
     bool got_event = false;
     while (keep_looping)
       {
         keep_looping = false;
-        got_event = queue_.deqTimedWait(p, waiting_time_);
+        got_event = incoming_events.deqTimedWait(p, waiting_time);
         if (!got_event) {
           mf::LogInfo("InputFailure")
             << "Reading timed out in RawEventQueueReader::readNext()";
-          keep_looping = resume_after_timeout_;
+          keep_looping = resume_after_timeout;
         }
       }
-    if (!got_event) return false;
-    // check for end of data stream
-    if (!p) { return false; }
+
+    // We return false, indicating we're done reading, if:
+
+    //   1) we did not obtain an event, because we timed out and were
+    //      configured NOT to keep trying after a timeout, or
+    //   2) the event we read was the end-of-data marker: a null
+    //      pointer
+
+    if (!got_event || !p) return false;
+
     art::Timestamp runstart;
     // make new runs or subruns if in* are 0 or if the run/subrun
     // have changed
     if (inR == 0 || inR->run() != p->runID()) {
-      outR = pm_.makeRunPrincipal(p->runID(),
-                                  runstart);
+      outR = pmaker.makeRunPrincipal(p->runID(),
+                                     runstart);
     }
     art::SubRunID subrun_check(p->runID(), p->subrunID());
     if (inSR == 0 || subrun_check != inSR->id()) {
-      outSR = pm_.makeSubRunPrincipal(p->runID(),
+      outSR = pmaker.makeSubRunPrincipal(p->runID(),
                                       p->subrunID(),
                                       runstart);
     }
-    outE = pm_.makeEventPrincipal(p->runID(),
+    outE = pmaker.makeEventPrincipal(p->runID(),
                                   p->subrunID(),
                                   p->eventID(),
                                   runstart);
+
     // add all the fragments as products
-    if (inst_names_.size() < p->fragments_.size()) {
+    if (product_instance_names.size() < p->fragments_.size()) {
       throw art::Exception(art::errors::DataCorruption)
         << "more raw data fragments than expected.\n"
-        << "expected " << inst_names_.size() << " and got "
+        << "expected " << product_instance_names.size() << " and got "
         << p->fragments_.size() << "\n"
         << "for event " << p->runID();
     }
@@ -125,7 +145,7 @@ namespace artdaq {
       // std::unique_ptr, and move semantics to move into and out of
       // the shared queue.
       auto_ptr<Fragment> frag(new Fragment(*p->fragments_[i]));
-      put_product_in_principal(frag, *outE, inst_names_[i]);
+      put_product_in_principal(frag, *outE, product_instance_names[i]);
     }
 
     return true;
