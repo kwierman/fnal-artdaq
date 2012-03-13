f651344c443fdc5eed6b29a24e1b1090fb969d2f
Chris Green <artists@fnal.gov>
2012-03-13 11:22:17 -0500

    New GenericFragmentSimulator class: UNTESTED -- tests to follow.

---
+++ artdaq/DAQrate/GenericFragmentSimulator.cc
@@ -0,0 +1,73 @@
+#include "artdaq/DAQrate/GenericFragmentSimulator.hh"
+#include "fhiclcpp/ParameterSet.h"
+
+#include <algorithm>
+#include <limits>
+#include <functional>
+
+artdaq::GenericFragmentSimulator::GenericFragmentSimulator(fhicl::ParameterSet const & ps) :
+  content_selection_(ps.get<content_selector_t>("content_selection",
+                                                content_selector_t::EMPTY)),
+  event_size_spec_(ps.get<size_t>("event_size", 10240)),
+  events_to_generate_(ps.get<size_t>("events_to_generate", 0)),
+  fragments_per_event_(ps.get<size_t>("fragments_per_event", 5)),
+  run_number_(ps.get<RawDataType>("run_number", 1)),
+  want_random_event_size_(ps.get<bool>("want_random_event_size", true)),
+  current_event_num_(0),
+  engine_(new CLHEP::HepJamesRandom(ps.get<int64_t>("random_seed", 314159))),
+  event_size_generator_(*engine_, event_size_spec_),
+  event_content_generator_(*engine_)
+{ }
+
+artdaq::GenericFragmentSimulator::~GenericFragmentSimulator()
+{ }
+
+bool
+artdaq::GenericFragmentSimulator::getNext_(FragmentPtrs & frags)
+{
+  ++current_event_num_;
+  if (events_to_generate_ != 0 &&
+      current_event_num_ > events_to_generate_) {
+    return false;
+  }
+  RawDataType fragID(0);
+  for (size_t i = 0; i < fragments_per_event_; ++i) {
+    ++fragID;
+    frags.emplace_back(new Fragment(current_event_num_, fragID));
+    size_t event_size = eventSize_();
+    frags.back()->reserve(event_size); // Allocate correct size now.
+    switch (content_selection_) {
+    case content_selector_t::EMPTY:
+      break;
+    case content_selector_t::FRAG_ID:
+      std::fill_n(frags.back()->dataBegin(), eventSize_(), fragID);
+      break;
+    case content_selector_t::RANDOM:
+      std::generate_n(frags.back()->dataBegin(),
+                      event_size,
+                      [&]() -> long {
+                        return
+                          event_content_generator_.fireInt(std::numeric_limits<long>::max());
+                      }
+                     );
+      break;
+    case content_selector_t::DEAD_BEEF:
+      std::fill_n(frags.back()->dataBegin(), event_size, 0XDEADBEEF);
+      break;
+    default:
+      throw cet::exception("UnknownContentSelection")
+        << "Unknown content selection: "
+        << static_cast<uint8_t>(content_selection_);
+    }
+  }
+  return true;
+}
+
+std::size_t
+artdaq::GenericFragmentSimulator::
+eventSize_()
+{
+  return want_random_event_size_?
+    event_size_generator_.fire():
+    event_size_spec_;
+}
---
+++ artdaq/DAQrate/GenericFragmentSimulator.hh
@@ -0,0 +1,52 @@
+#ifndef artdaq_DAQrate_GenericFragmentSimulator_hh
+#define artdaq_DAQrate_GenericFragmentSimulator_hh
+
+#include "CLHEP/Random/JamesRandom.h"
+#include "CLHEP/Random/RandFlat.h"
+#include "CLHEP/Random/RandPoissonT.h"
+
+#include "artdaq/DAQdata/Fragments.hh"
+#include "artdaq/DAQrate/FragmentGenerator.hh"
+#include "fhiclcpp/fwd.h"
+
+namespace artdaq {
+  class GenericFragmentSimulator;
+}
+
+// GenericFragmentSimulator creates simulated Generic events, with data
+// distributed according to a "histogram" provided in the configuration
+// data.
+
+class artdaq::GenericFragmentSimulator : public artdaq::FragmentGenerator {
+public:
+  explicit GenericFragmentSimulator(fhicl::ParameterSet const & ps);
+  virtual ~GenericFragmentSimulator();
+
+  enum class content_selector_t : uint8_t {
+    EMPTY = 0,
+    FRAG_ID,
+    RANDOM,
+    DEAD_BEEF
+  };
+
+private:
+  virtual bool getNext_(FragmentPtrs & output);
+
+  std::size_t eventSize_();
+
+  // Configuration
+  content_selector_t const content_selection_;
+  std::size_t const event_size_spec_; // Posson mean if random size wanted.
+  std::size_t const events_to_generate_; // Go forever if this is 0
+  std::size_t const fragments_per_event_;
+  RawDataType const run_number_;
+  bool const want_random_event_size_;
+
+// State
+  std::size_t current_event_num_;
+  std::unique_ptr<CLHEP::HepRandomEngine> engine_;
+  CLHEP::RandPoissonT event_size_generator_;
+  CLHEP::RandFlat event_content_generator_;
+};
+
+#endif /* artdaq_DAQrate_GenericFragmentSimulator_hh */
