45179f4a9bcbf73a23ca7cac28664b0e4424df32
Chris Green <artists@fnal.gov>
2012-03-14 10:17:44 -0500

    Add new info; correct namespace.

--- artdaq/DAQdata/detail/DS50Header.hh
+++ artdaq/DAQdata/detail/DS50Header.hh
@@ -1,28 +1,34 @@
 #ifndef artdaq_DAQdata_detail_DS50Header_hh
 #define artdaq_DAQdata_detail_DS50Header_hh
-namespace DS50 {
+namespace ds50 {
   namespace detail {
-    struct Header {
-      typedef uint32_t event_size_t;
-      typedef uint8_t channel_mask_t;
-      typedef uint16_t pattern_t;
-      typedef uint8_t  board_id_t;
-      typedef uint32_t event_counter_t;
-      typedef uint32_t trigger_time_tag_t;
-
-      uint32_t event_size : 28;
-      uint32_t unused_1   :  4;
-
-      uint32_t channel_mask :  8;
-      uint32_t pattern      : 16;
-      uint32_t unused_2     :  3;
-      uint32_t board_id     :  5;
-
-      uint32_t event_counter : 24;
-      uint32_t reserved      :  8;
-
-      uint32_t trigger_time_tag : 32;
-    };
+    struct Header;
   }
 }
+
+struct ds50::detail::Header {
+  typedef uint32_t data_t;
+
+  typedef uint32_t event_size_t;
+  typedef uint8_t channel_mask_t;
+  typedef uint16_t pattern_t;
+  typedef uint8_t  board_id_t;
+  typedef uint32_t event_counter_t;
+  typedef uint32_t trigger_time_tag_t;
+
+  uint32_t event_size : 28;
+  uint32_t unused_1   :  4;
+
+  uint32_t channel_mask :  8;
+  uint32_t pattern      : 16;
+  uint32_t unused_2     :  3;
+  uint32_t board_id     :  5;
+
+  uint32_t event_counter : 24;
+  uint32_t reserved      :  8;
+
+  uint32_t trigger_time_tag : 32;
+
+  static size_t const size_words = 4;
+};
 #endif /* artdaq_DAQdata_detail_DS50Header_hh */
