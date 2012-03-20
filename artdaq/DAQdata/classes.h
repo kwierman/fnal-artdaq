6476d59ce59c62335239cc8646494517e2800616
Chris Green <artists@fnal.gov>
2012-03-20 17:19:26 -0500

    Remove items from dictionary to help compilation.

--- artdaq/DAQdata/classes.h
+++ artdaq/DAQdata/classes.h
@@ -8,7 +8,5 @@ namespace {
   {
     std::vector<artdaq::Fragment> x3;
     art::Wrapper<std::vector<artdaq::Fragment> > x4;
-    ds50::DS50RawData x7;
-    art::Wrapper<ds50::DS50RawData> x8;
   };
 }
