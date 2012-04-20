dd5ca20340723d25def71f162e731d04f131087f
Chris Green <artists@fnal.gov>
2012-04-20 16:28:55 -0500

    Lots of new stuff for testing transfer / [de]compression including new decompression module.

---
+++ artdaq/ArtModules/DS50Decompression_module.cc
@@ -0,0 +1,114 @@
+#include "art/Framework/Core/EDProducer.h"
+#include "art/Framework/Core/ModuleMacros.h"
+#include "art/Framework/Principal/Event.h"
+#include "art/Framework/Principal/Handle.h"
+#include "art/Framework/Principal/Run.h"
+#include "art/Framework/Principal/SubRun.h"
+#include "art/Persistency/Provenance/BranchType.h"
+#include "artdaq/Compression/Decoder.hh"
+#include "artdaq/Compression/Properties.hh"
+#include "artdaq/Compression/SymTable.hh"
+#include "artdaq/DAQdata/DS50Board.hh"
+#include "artdaq/DAQdata/DS50RawData.hh"
+#include "artdaq/DAQdata/Fragment.hh"
+#include "artdaq/DAQdata/Fragments.hh"
+#include "cpp0x/memory"
+#include "fhiclcpp/ParameterSet.h"
+#include "messagefacility/MessageLogger/MessageLogger.h"
+
+#include <cmath>
+#include <iostream>
+#include <string>
+
+namespace {
+  template <class T>
+  T safe_rescale(T num, T div_by) {
+    return ceil(num / (static_cast<double>(div_by)));
+  }
+}
+
+namespace ds50 {
+
+  class DS50Decompression : public art::EDProducer {
+  public:
+    explicit DS50Decompression(fhicl::ParameterSet const & p);
+    virtual ~DS50Decompression() { }
+
+    virtual void produce(art::Event & e);
+    virtual void endSubRun(art::SubRun & sr);
+    virtual void endRun(art::Run & r);
+
+  private:
+    std::string compressed_label_;
+    std::string table_file_;
+    SymTable table_;
+    Decoder decode_;
+  };
+
+  static SymTable readAndSortTable(std::string const & fname)
+  {
+    SymTable t;
+    readTable(fname.c_str(), t, Properties::count_max());
+    std::sort(t.begin(),
+              t.end(),
+              [&](SymCode const & a,
+                  SymCode const & b) {
+                return a.bit_count_ < b.bit_count_;
+              });
+    return t;
+  }
+
+  DS50Decompression::DS50Decompression(fhicl::ParameterSet const & p)
+    : compressed_label_(p.get<std::string>("compressed_label")),
+      table_file_(p.get<std::string>("table_file")),
+      table_(readAndSortTable(table_file_)),
+      decode_(table_)
+  {
+    produces<artdaq::Fragments>();
+    std::cerr << "Hello from DS50Decompression\n";
+  }
+
+  void DS50Decompression::produce(art::Event & e)
+  {
+    art::Handle<DS50RawData> handle;
+    e.getByLabel(compressed_label_, handle);
+    // handle->dataBegin(), handle->dataEnd()
+    size_t len = handle->size();
+    std::auto_ptr<artdaq::Fragments> prod(new artdaq::Fragments(len));
+    #pragma omp parallel for \
+    shared(len, prod, handle)
+    for (size_t i = 0; i < len; ++i) {
+#ifndef NDEBUG
+      mf::LogDebug("Loop")
+        << "Attempting to decompress fragment # "
+        << i;
+#endif
+      artdaq::Fragment & newfrag = (*prod)[i];
+      newfrag = (handle->headerOnlyFrag(i)); // Load in the header
+      Board b(newfrag);
+      static size_t const
+        fToWFactor(sizeof(artdaq::Fragment::value_type) /
+                   sizeof(Board::data_t));
+      newfrag.resize(b.event_size() / fToWFactor); // To hold decompressed data.
+      static size_t const
+        wToAFactor(sizeof(Board::data_t) / sizeof(adc_type));
+      auto adc_start = reinterpret_cast<adc_type *>
+                       (&*newfrag.dataBegin() +
+                        safe_rescale(Board::header_size_words(), wToAFactor));
+      auto adc_end   = b.dataEnd();
+      auto size_check __attribute__((unused))
+        (decode_(handle->fragmentBitCount(i),
+                 handle->fragment(i).begin(),
+                 adc_start,
+                 adc_end));
+      assert(size_check == newfrag.dataSize() - Board::header_size_words());
+    }
+    e.put(prod);
+  }
+
+  void DS50Decompression::endSubRun(art::SubRun &) { }
+  void DS50Decompression::endRun(art::Run &) { }
+
+  DEFINE_ART_MODULE(DS50Decompression)
+}
+
---
+++ test/ArtModules/DS50CompressionChecker_module.cc
@@ -0,0 +1,67 @@
+////////////////////////////////////////////////////////////////////////
+// Class:       DS50CompressionChecker
+// Module Type: analyzer
+// File:        DS50CompressionChecker_module.cc
+//
+// Generated at Mon Apr 16 11:46:47 2012 by Christopher Green using artmod
+// from art v0_00_02.
+////////////////////////////////////////////////////////////////////////
+
+#include "art/Framework/Core/EDAnalyzer.h"
+#include "art/Framework/Core/ModuleMacros.h"
+#include "art/Framework/Principal/Event.h"
+#include "art/Framework/Principal/Handle.h"
+#include "artdaq/DAQdata/Fragments.hh"
+
+#include <boost/test/included/unit_test.hpp>
+
+#include <algorithm>
+#include <cassert>
+
+using artdaq::Fragment;
+using artdaq::Fragments;
+
+namespace ds50test {
+  class DS50CompressionChecker;
+}
+
+class ds50test::DS50CompressionChecker : public art::EDAnalyzer {
+public:
+  explicit DS50CompressionChecker(fhicl::ParameterSet const & p);
+  virtual ~DS50CompressionChecker();
+
+  virtual void analyze(art::Event const & e);
+
+private:
+
+    std::string raw_label_;
+    std::string uncompressed_label_;
+};
+
+
+ds50test::DS50CompressionChecker::DS50CompressionChecker(fhicl::ParameterSet const & p)
+  :
+  raw_label_(p.get<std::string>("raw_label")),
+  uncompressed_label_(p.get<std::string>("uncompressed_label"))
+{
+}
+
+ds50test::DS50CompressionChecker::~DS50CompressionChecker()
+{
+}
+
+void ds50test::DS50CompressionChecker::analyze(art::Event const & e)
+{
+  art::Handle<artdaq::Fragments> raw, uncomp;
+  assert(e.getByLabel(raw_label_, raw));
+  assert(e.getByLabel(uncompressed_label_, uncomp));
+  assert(raw->size() == uncomp->size());
+  for (size_t i = 0; i < raw->size(); ++i) {
+    Fragment const & rf ((*raw)[i]);
+    Fragment const & uf ((*uncomp)[i]);
+    assert(rf.size() == uf.size());
+    assert(std::equal(rf.dataBegin(), rf.dataEnd(), uf.dataBegin()));
+  }
+}
+
+DEFINE_ART_MODULE(ds50test::DS50CompressionChecker)
