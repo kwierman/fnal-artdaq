b14f50a59123dcd9227a2a9ba3b0887803b335ef
Marc Paterno <paterno@fnal.gov>
2012-11-14 09:56:20 -0700

    Add more comments

--- artdaq/DAQrate/SHandles.hh
+++ artdaq/DAQrate/SHandles.hh
@@ -9,14 +9,8 @@
 
 #include "artdaq/DAQrate/quiet_mpi.hh"
 
-/*
-  Protocol: want to do a send for each request object, then wait for for
-  pending requests to complete, followed by a reset to allow another set
-  of sends to be completed.
-
-  This needs to be separated into a thing for sending and a thing for receiving.
-  There probably needs to be a common class that both use.
- */
+// SHandles handles the transmission of Fragments to one or more receivers in a
+// round-robin fashion. Multiple receivers must have consecutive rank numbers.
 
 namespace artdaq {
   class SHandles;
@@ -28,14 +22,26 @@ public:
   typedef std::vector<MPI_Status> Statuses;
   typedef std::vector<int> Flags; // busy flags
 
+  // buffer_count is the number of MPI_Request objects that will be used.
+  // Fragments with dataSize() greater than max_payload_size will not be send.
+  // dest_count is the number of receivers used in the round-robin algorithm
+  // dest_start is the rank of the first receiver
   SHandles(size_t buffer_count,
            uint64_t max_payload_size,
            size_t dest_count,
            size_t dest_start);
 
-  size_t sendFragment(Fragment &&); // Return dest.
-  void sendEODFrag(int dest, size_t nFragments);
+  // Send the given Fragment. Return the rank of the destination to which
+  // the Fragment was sent.
+  size_t sendFragment(Fragment &&);
 
+  // Send an EOF Fragment to the receiver at rank dest;
+  // the EOF Fragment will report that numFragmentsSent
+  // fragments have been sent before this one.
+  void sendEODFrag(int dest, size_t numFragmentsSent);
+
+  // Wait for all the data transfers scheduled by calls
+  // to MPI_Isend to finish, then return.
   void waitAll();
 
 private:
