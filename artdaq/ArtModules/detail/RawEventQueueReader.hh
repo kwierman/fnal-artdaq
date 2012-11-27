113b8c9fc35e0c025555d94d76d5a616d06d88fe
Chris Green <artists@fnal.gov>
2012-11-27 17:10:04 -0600

    Cope with changes in art v1_02_06.

--- artdaq/ArtModules/detail/RawEventQueueReader.hh
+++ artdaq/ArtModules/detail/RawEventQueueReader.hh
@@ -6,6 +6,7 @@
 #include "art/Framework/Core/FileBlock.h"
 #include "art/Framework/Core/PrincipalMaker.h"
 #include "art/Framework/Core/ProductRegistryHelper.h"
+#include "art/Framework/IO/Sources/SourceTraits.h"
 #include "art/Framework/Principal/EventPrincipal.h"
 #include "art/Framework/Principal/RunPrincipal.h"
 #include "art/Framework/Principal/SubRunPrincipal.h"
@@ -40,7 +41,16 @@ namespace artdaq {
                     art::EventPrincipal *& outE);
     };
 
-  }
+  } // detail
+} // artdaq
+
+// Specialize an art source trait to tell art that we don't care about
+// source.fileNames and don't want the files services to be used.
+namespace art {
+  template <>
+  struct Source_generator<artdaq::detail::RawEventQueueReader> {
+    static constexpr bool value = true;
+  };
 }
 
 #endif /* artdaq_ArtModules_detail_RawEventQueueReader_hh */
