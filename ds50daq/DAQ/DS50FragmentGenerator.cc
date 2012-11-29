a3f87ec4ce91d44ec9b2ff41e46b5fded8d19865
Alessandro Razeto <razeto@lngs.infn.it>
2012-11-29 15:16:24 -0600

    Stop is no more blocking. Construct with ParameterSet for fragment_id and sleep_us (opt).

--- ds50daq/DAQ/DS50FragmentGenerator.cc
+++ ds50daq/DAQ/DS50FragmentGenerator.cc
@@ -1,9 +1,12 @@
 #include "DS50FragmentGenerator.hh"
+#include "fhiclcpp/ParameterSet.h"
 #include <boost/lexical_cast.hpp>
-
 #include "cetlib/exception.h"
+#include <unistd.h>
 
-ds50::DS50FragmentGenerator::DS50FragmentGenerator(): run_number_(-1) {
+ds50::DS50FragmentGenerator::DS50FragmentGenerator(const fhicl::ParameterSet &ps): run_number_(-1) {
+  fragment_id_ = ps.get<int> ("fragment_id");
+  sleep_us_ = ps.get<int> ("sleep_us", 0);
 }
 
 void ds50::DS50FragmentGenerator::start (int run) { 
@@ -25,22 +28,18 @@ void ds50::DS50FragmentGenerator::resume () { resume_ (); }
 void ds50::DS50FragmentGenerator::stop () { 
   std::unique_lock<std::mutex> lk(mutex_);
   should_stop_ = true;
-  stats_.should_stop = true;
   stop_ (); 
-
-  stop_wait_.wait (lk, [&](){ return !should_stop_; });
 }
 
 std::string ds50::DS50FragmentGenerator::report () { 
   std::lock_guard<std::mutex> lk(mutex_);
-  return boost::lexical_cast<std::string>(stats_.should_stop) + report_ ();
+  return boost::lexical_cast<std::string>(stats_.read_count) + report_ ();
 }
 
 void ds50::DS50FragmentGenerator::perfreset () {
   std::lock_guard<std::mutex> lk(mutex_);
-  stats_.should_stop = should_stop_;
   stats_.run_number = run_number_;
-  stats_.call_count = 0;
+  stats_.read_count = stats_.call_count = 0;
   stats_.avg_frag_size = 0;
   perfreset_ ();
 }
@@ -51,18 +50,17 @@ bool ds50::DS50FragmentGenerator::should_stop () { return should_stop_; } // no 
 bool ds50::DS50FragmentGenerator::getNext_ (artdaq::FragmentPtrs & output) { 
   if (run_number_ < 0) start (0);
 
+  if (sleep_us_ > 0) usleep (sleep_us_);
+
   std::lock_guard<std::mutex> lk(mutex_);
+
+  stats_.read_count -= output.size ();
 
   bool r = getNext__(output); 
 
-  if (!r) {
-    should_stop_ = false;
-    stop_wait_.notify_all ();
-  }
-
   stats_.avg_frag_size = (stats_.avg_frag_size * stats_.call_count + output.size ()) / (stats_.call_count + 1);
   stats_.call_count ++;
-  stats_.should_stop = should_stop_;
+  stats_.read_count += output.size ();
 
   return r;
 }
