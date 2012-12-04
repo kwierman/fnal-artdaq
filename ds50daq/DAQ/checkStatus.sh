64b090f8ca949a7c0f25ba13509784a92f12affe
Kurt Biery <biery@fnal.gov>
2012-12-04 13:58:17 -0600

    script to check 2x2 application states

---
+++ ds50daq/DAQ/checkStatus.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+xmlrpc http://localhost:5440/RPC2 ds50.status
+xmlrpc http://localhost:5441/RPC2 ds50.status
+
+xmlrpc http://localhost:5450/RPC2 ds50.status
+xmlrpc http://localhost:5451/RPC2 ds50.status
