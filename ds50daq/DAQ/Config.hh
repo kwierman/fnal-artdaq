96c2e8e1f5de47ce8143fd1489fc1b82831e5f2b
Kurt Biery <biery@fnal.gov>
2013-02-19 10:15:49 +0100

    Added initial fragment types

--- ds50daq/DAQ/Config.hh
+++ ds50daq/DAQ/Config.hh
@@ -1,11 +1,16 @@
 #ifndef ds50daq_DAQ_Config_hh
 #define ds50daq_DAQ_Config_hh
 
+#include "artdaq/DAQdata/detail/RawFragmentHeader.hh"
+
 namespace ds50
 {
   namespace Config
   {
     enum TaskType : int {FragmentReceiverTask=1, EventBuilderTask=2, AggregatorTask=3};
+
+    static const artdaq::detail::RawFragmentHeader::type_t V172X_FRAGMENT_TYPE = 17;
+    static const artdaq::detail::RawFragmentHeader::type_t V1495_FRAGMENT_TYPE = 18;
   }
 }
 
