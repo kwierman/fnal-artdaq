41ba8ddc4e4f467bc656ae2c857efcace5f1d2fd
Kurt Biery <biery@fnal.gov>
2013-03-07 09:51:45 -0600

    Updated Fragment type values

--- ds50daq/DAQ/Config.hh
+++ ds50daq/DAQ/Config.hh
@@ -9,8 +9,10 @@ namespace ds50
   {
     enum TaskType : int {FragmentReceiverTask=1, EventBuilderTask=2, AggregatorTask=3};
 
-    static const artdaq::detail::RawFragmentHeader::type_t V172X_FRAGMENT_TYPE = 17;
-    static const artdaq::detail::RawFragmentHeader::type_t V1495_FRAGMENT_TYPE = 18;
+    static const artdaq::detail::RawFragmentHeader::type_t MISSED_FRAGMENT_TYPE = 1;
+    static const artdaq::detail::RawFragmentHeader::type_t V1495_FRAGMENT_TYPE =  2;
+    static const artdaq::detail::RawFragmentHeader::type_t V1720_FRAGMENT_TYPE =  3;
+    static const artdaq::detail::RawFragmentHeader::type_t V1724_FRAGMENT_TYPE =  4;
   }
 }
 
