e65d16d7fc6ac49f0319468534acd4df7ba77e7b
Steve Foulkes <sfoulkes@fnal.gov>
2013-03-27 21:04:38 +0100

    Initial revisions of headers for the Fragment class so that they can transport data for the NetMon input/output modules.

---
+++ artdaq/DAQdata/NetMonHeader.cc
@@ -0,0 +1,13 @@
+#include "artdaq/DAQdata/detail/RawFragmentHeader.hh"
+#include "artdaq/DAQdata/NetMonHeader.hh"
+
+using artdaq::detail::RawFragmentHeader;
+
+artdaq::NetMonHeader::type_t const artdaq::NetMonHeader::EventDataFragmentType =
+  detail::RawFragmentHeader::FIRST_USER_TYPE;
+artdaq::NetMonHeader::type_t const artdaq::NetMonHeader::InitDataFragmentType =
+  detail::RawFragmentHeader::FIRST_USER_TYPE + 1;
+artdaq::NetMonHeader::type_t const artdaq::NetMonHeader::RunDataFragmentType =
+  detail::RawFragmentHeader::FIRST_USER_TYPE + 2;
+artdaq::NetMonHeader::type_t const artdaq::NetMonHeader::SubRunDataFragmentType =
+  detail::RawFragmentHeader::FIRST_USER_TYPE + 3;
