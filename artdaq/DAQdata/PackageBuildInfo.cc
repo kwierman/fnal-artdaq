b27aa1b07a78b968c61a72bf629c8fdac0308974
Kurt Biery <biery@fnal.gov>
2014-04-30 09:39:25 -0500

    Adding PackageBuildInfo to provide persistent storage of package version and build time information.

---
+++ artdaq/DAQdata/PackageBuildInfo.cc
@@ -0,0 +1,5 @@
+#include "artdaq/DAQdata/PackageBuildInfo.hh"
+
+namespace artdaq {
+  PackageBuildInfo::PackageBuildInfo() {}
+}
--- artdaq/DAQdata/classes.h
+++ artdaq/DAQdata/classes.h
@@ -1,6 +1,8 @@
 #include "artdaq/DAQdata/Fragment.hh"
 #include "art/Persistency/Common/Wrapper.h"
+#include "artdaq/DAQdata/PackageBuildInfo.hh"
 #include <vector>
 
 template class std::vector<artdaq::Fragment>;
 template class art::Wrapper<std::vector<artdaq::Fragment> >;
+template class art::Wrapper<artdaq::PackageBuildInfo>;
