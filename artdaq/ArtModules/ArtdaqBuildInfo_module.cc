b32d6b9051a605d8ac1e7922fa28f02d057e4cb3
John Freeman <jcfree@fnal.gov>
2014-09-11 15:27:42 -0500

    Moved the BuildInfo template code out (and into artdaq-core); added artdaq-core package info to the ArtdaqBuildInfo module

--- artdaq/ArtModules/ArtdaqBuildInfo_module.cc
+++ artdaq/ArtModules/ArtdaqBuildInfo_module.cc
@@ -1,13 +1,14 @@
-#include "artdaq/ArtModules/BuildInfo_module.hh"
+#include "artdaq-core/ArtModules/BuildInfo_module.hh"
 
 #include "artdaq/BuildInfo/GetPackageInfo.h"
+#include "artdaq-core/BuildInfo/GetPackageInfo.h"
 
 #include <string>
 
 namespace artdaq {
 
   static std::string instanceName = "ArtdaqBuildInfo";
-  typedef artdaq::BuildInfo< &instanceName, artdaq::PackageInfo> ArtdaqBuildInfo;
+  typedef artdaq::BuildInfo< &instanceName, artdaqcore::PackageInfo, artdaq::PackageInfo> ArtdaqBuildInfo;
 
   DEFINE_ART_MODULE(ArtdaqBuildInfo)
 }
